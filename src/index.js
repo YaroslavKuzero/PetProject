1154ea9820728e2b603a85aee1e0aae826c49603
Maxim Kozin <maxa.kozin@gmail.com>
2020-07-01 14:16:29 +0300

    addet tab switcher to daily weather box

--- src/index.js
+++ src/index.js
@@ -18,9 +18,38 @@ const markupCurrent = curr(current);
 refs.current.insertAdjacentHTML("beforeend", markupCurrent);
 const markupDaily = day(daily);
 refs.daily.insertAdjacentHTML("beforeend", markupDaily);
+refs.daily.firstElementChild.classList.add('tab--active')
 const markupBtns = btns(daily);
 refs.btns.insertAdjacentHTML('beforeend', markupBtns);
+refs.btns.firstElementChild.classList.add('daily--active');
 const markupHourly = hrly(hourly);
 refs.hourly.insertAdjacentHTML('beforeend', markupHourly);
 const markupLogo = logo();
 refs.root.insertAdjacentHTML('afterbegin', markupLogo);
+
+const tabSwitcher = function (event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'A') {
+    console.log('Ne tuda');
+    return;
+  };
+
+  const activeBtn = refs.btns.querySelector('.daily--active');
+
+  if (activeBtn) {
+    activeBtn.classList.remove('daily--active');
+    const tabId = activeBtn.getAttribute('href').slice(1);
+    const tab = refs.daily.querySelector(`#${tabId}`);
+    tab.classList.remove('tab--active');
+  };
+
+  const controlItem = event.target;
+  const tabId = controlItem.getAttribute('href').slice(1);
+  controlItem.classList.add('daily--active');
+
+  const tab = refs.daily.querySelector(`#${tabId}`);
+  tab.classList.add('tab--active');
+}
+
+refs.btns.addEventListener('click', tabSwitcher);
